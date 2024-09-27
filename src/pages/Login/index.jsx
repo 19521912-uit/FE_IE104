c66d0c631aeaf6bf4849e5ecdac65134f4a73c78
Nguyễn Viết Công <91941271+nvcong1710@users.noreply.github.com>
2024-09-27 16:07:07 +0700

    feat -config project

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export default function Login() {
+    return (
+        <>login</>
+    );
+}
---
+++ src/pages/Register/index.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export default function Register() {
+
+    return (
+        <div>
+            Register
+        </div>
+    );
+}
