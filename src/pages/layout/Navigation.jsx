dae00f7d75a2ee7afe97524acab63013dc88f15e
Nguyễn Viết Công <91941271+nvcong1710@users.noreply.github.com>
2024-10-02 14:07:53 +0700

    add footer and navigation file, add default layout

---
+++ src/pages/layout/Navigation.jsx
@@ -0,0 +1,13 @@
+import React, { useContext, useState } from 'react';
+import { FaHome, FaUser, FaSignInAlt, FaShoppingCart, FaWallet, FaSignOutAlt, FaCog, FaInfoCircle, FaChevronDown } from 'react-icons/fa';
+import AuthContext from './../context/AuthContext';
+
+const Navigation = () => {
+    return (
+        <nav className="navigation">
+            Navigation
+        </nav>
+    );
+};
+
+export default Navigation;
